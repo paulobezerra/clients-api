b1ad11024bb7d3df9816f98bcea2e0a3cef81e34
Paulo Bezerra <bezerra.paulo@gmail.com>
2022-02-25 19:30:52 -0400

    First commit

---
+++ src/test/java/io/platformbuilders/clients/ClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.platformbuilders.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
